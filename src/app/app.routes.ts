23b1cebdec5c0e5e261ae69244c5a7746e40a274
Heet Doshi <doshiheet2872003@gmail.com>
2024-04-25 16:04:18 +0530

    RXJS concept

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RxjsLearnComponent } from './rxjs-learn/rxjs-learn.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', redirectTo:'rxjs-concept',pathMatch:'full'},
+  {path:'rxjs-concept',component:RxjsLearnComponent}
+];
