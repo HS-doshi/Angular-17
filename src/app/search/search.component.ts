02c690d06f42288a487acc894eecaba530a296bd
Heet Doshi <doshiheet2872003@gmail.com>
2024-04-29 12:17:28 +0530

    learning

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,6 +3,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Observable, debounceTime, distinct, elementAt, filter, first, from, last, of, skip, take, takeLast, takeWhile } from 'rxjs';
 
+function TransformValue(value:string){
+  return 'Hii '+ value.split(' ').map(upper=>upper.charAt(0).toUpperCase()+ upper.slice(1)).join(' ')
+}
+
 @Component({
   selector: 'app-search',
   standalone: true,
@@ -10,13 +14,18 @@ import { Observable, debounceTime, distinct, elementAt, filter, first, from, las
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
+
 export class SearchComponent implements OnInit{
   searchForm!:FormGroup;
   name!:FormControl;
   compant =['Wellnest','RoyalNest','Royal','Cheese','RoyalNest','Royal','WellCheese']
   company$ :Observable<string> = from(this.compant)
 
-  @Input() names =''
+  // Input decorator useful for recieve data from parent to child!
+  // @output decorator will useful for pass data from child to parent!
+
+  // alias is used for as we given another name of in our parent component! -we pass here for access those data.
+  @Input({alias:'userName',transform:TransformValue}) names =''
   constructor( private formBuilder:FormBuilder){}
   ngOnInit(): void {
     this.searchForm = new FormGroup({
