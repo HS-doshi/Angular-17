b3ba71b756aa18ffc476991cfe506578bf75de9c
Heet Doshi <doshiheet2872003@gmail.com>
2024-05-01 11:14:27 +0530

    Firebase removed

--- firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAR5Zxt-nqhcz3FSaTtT108-pE2sA5VXUw",
-    authDomain: "codebin-ng-83577.firebaseapp.com",
-    projectId: "codebin-ng-83577",
-    storageBucket: "codebin-ng-83577.appspot.com",
-    messagingSenderId: "1045924524616",
-    appId: "1:1045924524616:web:1d697a74817e72133a1e7d"
-  };
