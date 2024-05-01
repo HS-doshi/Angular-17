72d4323e8be3d216f3db32870e79adc6f53166a9
Heet Doshi <doshiheet2872003@gmail.com>
2024-05-01 11:37:40 +0530

    firebase done!

---
+++ firebaseConfig.ts
@@ -0,0 +1,9 @@
+import { environment } from "./environments.prod";
+export const firebaseConfig = {
+    apiKey: environment.apiKey,
+    authDomain: "codebin-ng-83577.firebaseapp.com",
+    projectId: "codebin-ng-83577",
+    storageBucket: "codebin-ng-83577.appspot.com",
+    messagingSenderId: "1045924524616",
+    appId: "1:1045924524616:web:1d697a74817e72133a1e7d"
+  };
